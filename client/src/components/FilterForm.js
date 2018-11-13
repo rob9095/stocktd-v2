7cae5a19e57e14fa918434a32450456ccf41fb2a
roberttucker9095@gmail.com <robt9095@gmail.com>
2018-11-12 17:10:21 -0800

    moved scanForm into own component

--- client/src/components/FilterForm.js
+++ client/src/components/FilterForm.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Form, Row, Col, Input, Button, Select, Switch, DatePicker, Icon } from 'antd';
-import { getAllModelDocuments, upsertModelDocuments } from '../store/actions/models';
-import InsertDataModal from './InsertDataModal';
+import WrappedScanForm from './ScanForm';
 
 const Option = Select.Option;
 const FormItem = Form.Item;
@@ -9,44 +8,13 @@ const { RangePicker } = DatePicker;
 
 class FilterForm extends Component {
   state = {
-    showDataModal: false,
     showFilterForm: false,
     showScannerForm: false,
-    boxPrefixList: [
-      {value: this.props.currentUser.email.split('@')[0], id: this.props.currentUser.id},     
-    ],
     selects: {
 
     },
     dates: [],
   };
-
-  getBoxPrefixes = async () => {
-    // get the box prefixes for this user
-    await getAllModelDocuments('BoxPrefix',{user: this.props.currentUser.id},this.props.currentUser.company)
-    .then(res=>{
-      let userPrefixList = res.data.map(pf => ({
-        value: pf.name,
-        id: pf._id,
-      }))
-      this.setState({
-        boxPrefixList: [...this.state.boxPrefixList, ...userPrefixList, {value: 'Add New', id: 'Add New'}],
-      })
-    })
-    .catch(err=>{
-      console.log(err)
-      this.setState({
-        boxPrefixList: [...this.state.boxPrefixList, {value: 'Add New', id: 'Add New'}],
-      })
-    })    
-  }
-
-  componentDidMount() {
-    if (this.props.showScannerForm) {
-      this.getBoxPrefixes()
-      console.log(this.state.boxPrefixList)
-    }
-  }
 
   toggle = (prop) => {
     return () => {
@@ -99,40 +67,8 @@ class FilterForm extends Component {
     });
   }
 
-  handlePrefixSelect = (value, option) => {
-    if (value === "Add New") {
-      this.setState({
-        showDataModal: true,
-      })
-    }
-  }
-
-  handleNewBoxPrefix = (data) => {
-    return new Promise((resolve,reject) => {
-      upsertModelDocuments('BoxPrefix', [{...data, user: this.props.currentUser.id}], this.props.currentUser.company, 'name')
-      .then(res => {
-        resolve({text:'Box Prefix Added',status:'success'})
-        this.setState({
-          boxPrefixList: [{value: data.name, id: res.upsertedDocs.upserted[0]._id}, ...this.state.boxPrefixList]
-        })
-      })
-      .catch(err => {
-        console.log(err)
-        reject({text:'Failed to Add Box Prefix',status:'error'})
-      })
-    })
-  }
-
   render() {
     const { getFieldDecorator } = this.props.form;
-    let preFixOptions = this.state.boxPrefixList.map(pf => (
-      <Option value={pf.value} key={pf.id}>{pf.value}</Option>
-    ))
-    let boxSelect = (
-      <Select onSelect={this.handlePrefixSelect} style={{minWidth: 100}} defaultValue={this.state.boxPrefixList[0].value}>
-        {preFixOptions}
-      </Select>
-    );
     let inputs = this.props.inputs.map(i=>{
       const selectBefore = (
         <Select key={`${i.id}Select`} defaultValue={'='} onChange={this.handleSelect} showArrow={false} className="number-input pre-select">
@@ -198,19 +134,6 @@ class FilterForm extends Component {
     })
     return (
       <div>
-        {this.state.showDataModal && (
-          <InsertDataModal
-            currentUser={this.props.currentUser}
-            title={'Add Box Prefix'}
-            inputs={[
-              {span: 24, id: 'name', text: 'Box Prefix', required: true, message: 'Box Prefix is required'},
-            ]}
-            okText={'Save'}
-            cancelText={'Cancel'}
-            onClose={this.toggle('showDataModal')}
-            onSave={this.handleNewBoxPrefix}
-          />
-        )}
         <Form layout="inline" style={{width: '10%', display: 'inline'}}>
           <FormItem label="Search">
             <Switch checked={this.state.showFilterForm} onChange={this.toggle('showFilterForm')} />
@@ -238,63 +161,10 @@ class FilterForm extends Component {
           </Form>
         )}
         {this.state.showScannerForm && (
-          <Form
-            className="scan-form"
-            onSubmit={this.handleScan}
-          >
-            <Row gutter={24}>
-              <Col s={24} md={8}>
-              <FormItem label="Box Name">
-                {getFieldDecorator('boxName', {
-                  rules: [{
-                    required: true,
-                    message: 'Box Name Required',
-                  }],
-                })(
-                  <Input addonBefore={boxSelect} placeholder="Box Name" />
-                )}
-              </FormItem>
-              </Col>
-              <Col s={24} md={10}>
-              <FormItem label="Scan ID">
-                {getFieldDecorator('scanId', {
-                  rules: [{
-                    required: true,
-                    message: 'Scan ID Required',
-                  }],
-                })(
-                  <Input placeholder="Scan ID" />
-                )}
-              </FormItem>
-              </Col>
-              <Col s={24} md={4}>
-              <FormItem label="Quantity">
-                {getFieldDecorator('scanQuantity', {initialValue: '1' }, {
-                  rules: [{
-                    required: true,
-                    message: 'Scan Quantity Required',
-                  }],
-                })(
-                  <Input type="number" />
-                )}
-              </FormItem>
-              </Col>
-              <Col s={24} md={2} style={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  height: 113,
-                }}>
-                <Button
-                type="primary"
-                htmlType="submit"
-                >
-                  Scan
-                </Button>
-              </Col>
-            </Row>
-          </Form>
-        )}        
+          <WrappedScanForm
+            currentUser={this.props.currentUser}
+          />
+        )}
       </div>
     );
   }
---
+++ client/src/components/ScanForm.js
@@ -0,0 +1,160 @@
+import React, { Component } from 'react';
+import { Form, Row, Col, Input, Button, Select } from 'antd';
+import { getAllModelDocuments, upsertModelDocuments } from '../store/actions/models';
+import InsertDataModal from './InsertDataModal';
+
+const Option = Select.Option;
+const FormItem = Form.Item;
+
+class ScanForm extends Component {
+  state = {
+    showDataModal: false,
+    boxPrefixList: [
+      {value: this.props.currentUser.email.split('@')[0], id: this.props.currentUser.id},     
+    ],
+  };
+
+  getBoxPrefixes = async () => {
+    // get the box prefixes for this user
+    await getAllModelDocuments('BoxPrefix',{user: this.props.currentUser.id},this.props.currentUser.company)
+    .then(res=>{
+      console.log(res)
+      let userPrefixList = res.data.map(pf => ({
+        value: pf.name,
+        id: pf._id,
+      }))
+      this.setState({
+        boxPrefixList: [...this.state.boxPrefixList, ...userPrefixList, {value: 'Add New', id: 'Add New'}],
+      })
+    })
+    .catch(err=>{
+      console.log(err)
+      this.setState({
+        boxPrefixList: [...this.state.boxPrefixList, {value: 'Add New', id: 'Add New'}],
+      })
+    })    
+  }
+
+  componentDidMount() {
+    this.getBoxPrefixes()
+  }
+
+  toggle = (prop) => {
+    return () => {
+      this.setState({
+        [prop]: !this.state[prop],
+      })
+    }
+  }
+
+  handlePrefixSelect = (value, option) => {
+    if (value === "Add New") {
+      this.setState({
+        showDataModal: true,
+      })
+    }
+  }
+
+  handleNewBoxPrefix = (data) => {
+    return new Promise((resolve,reject) => {
+      upsertModelDocuments('BoxPrefix', [{...data, user: this.props.currentUser.id}], this.props.currentUser.company, 'name')
+      .then(res => {
+        resolve({text:'Box Prefix Added',status:'success'})
+        this.setState({
+          boxPrefixList: [{value: data.name, id: res.upsertedDocs.upserted[0]._id}, ...this.state.boxPrefixList]
+        })
+      })
+      .catch(err => {
+        console.log(err)
+        reject({text:'Failed to Add Box Prefix',status:'error'})
+      })
+    })
+  }
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    let preFixOptions = this.state.boxPrefixList.map(pf => (
+      <Option value={pf.value} key={pf.id}>{pf.value}</Option>
+    ))
+    let boxSelect = (
+      <Select onSelect={this.handlePrefixSelect} style={{minWidth: 100}} defaultValue={this.state.boxPrefixList[0].value}>
+        {preFixOptions}
+      </Select>
+    );
+    return (
+      <div>
+        {this.state.showDataModal && (
+          <InsertDataModal
+            currentUser={this.props.currentUser}
+            title={'Add Box Prefix'}
+            inputs={[
+              {span: 24, id: 'name', text: 'Box Prefix', required: true, message: 'Box Prefix is required'},
+            ]}
+            okText={'Save'}
+            cancelText={'Cancel'}
+            onClose={this.toggle('showDataModal')}
+            onSave={this.handleNewBoxPrefix}
+          />
+        )}
+        <Form
+            className="scan-form"
+          >
+            <Row gutter={24}>
+              <Col s={24} md={8}>
+              <FormItem label="Box Name">
+                {getFieldDecorator('boxName', {
+                  rules: [{
+                    required: true,
+                    message: 'Box Name Required',
+                  }],
+                })(
+                  <Input addonBefore={boxSelect} placeholder="Box Name" />
+                )}
+              </FormItem>
+              </Col>
+              <Col s={24} md={10}>
+              <FormItem label="Scan ID">
+                {getFieldDecorator('scanId', {
+                  rules: [{
+                    required: true,
+                    message: 'Scan ID Required',
+                  }],
+                })(
+                  <Input placeholder="Scan ID" />
+                )}
+              </FormItem>
+              </Col>
+              <Col s={24} md={4}>
+              <FormItem label="Quantity">
+                {getFieldDecorator('scanQuantity', {initialValue: '1' }, {
+                  rules: [{
+                    required: true,
+                    message: 'Scan Quantity Required',
+                  }],
+                })(
+                  <Input type="number" />
+                )}
+              </FormItem>
+              </Col>
+              <Col s={24} md={2} style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  height: 113,
+                }}>
+                <Button
+                type="primary"
+                htmlType="submit"
+                >
+                  Scan
+                </Button>
+              </Col>
+            </Row>
+          </Form>       
+      </div>
+    );
+  }
+}
+
+const WrappedScanForm = Form.create()(ScanForm);
+export default WrappedScanForm
