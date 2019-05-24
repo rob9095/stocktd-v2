bca087ca6555bbf0bd316d120a1a725863313d83
roberttucker9095@gmail.com <robt9095@gmail.com>
2019-05-24 16:27:54 -0700

    update to notifications reducer to avoid adding notifications iwth the same id

--- client/src/store/reducers/notifications.js
+++ client/src/store/reducers/notifications.js
@@ -1,12 +1,9 @@
 import { ADD_NOTIFICATION, REMOVE_NOTIFICATION } from '../actionTypes';
 
 export default (state = [], action) => {
-  if (action.config) {
-    console.log(action)
-  }
   switch (action.type) {
     case ADD_NOTIFICATION:
-      return [action.config, ...state];
+      return state.find(n => n.id === action.config.id) ? [action.config, ...state.filter(n => n.id !== action.config.id)] : [action.config, ...state];
     case REMOVE_NOTIFICATION:
       return state.filter(n=>n.id !== action.config.id)
     default:
